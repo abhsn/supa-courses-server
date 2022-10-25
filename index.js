73139d3cc5a17183dfa747a85f0b7d9cd5787e64
Abid Hasan <ab1dhasan@outlook.com>
2022-10-25 14:03:27 +0600

    added start script to package.json

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const courses = require("./data/courses.json");
+
 app.get('/', (req, res) => {
 	res.send("Supa Courses API running...");
+});
+
+app.get('/api/courses', (req, res) => {
+	res.send(courses);
 });
 
 app.listen(port, () => console.log("Listening on port", port));
